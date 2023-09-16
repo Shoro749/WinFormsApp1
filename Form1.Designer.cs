25a4f6b4c13fd4e9eaf1b9143e78d173a53aa630
Shoro749 <romanbro749@gmail.com>
2023-09-16 17:49:28 +0300

    Task 3 completed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -21,7 +21,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(189, 158);
+            button1.Name = "button1";
+            button1.Size = new Size(395, 195);
+            button1.TabIndex = 0;
+            button1.Text = "\r\nПрямокутник";
+            button1.UseVisualStyleBackColor = true;
+            button1.MouseClick += MouseCoordinates;
+            button1.MouseDown += RightClick;
+            button1.MouseMove += MouseMove;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(269, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(467, 54);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 2;
+            label2.Text = "label2";
             // 
             // Form1
             // 
@@ -29,12 +62,20 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonShadow;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Application";
             Load += MainWindowLoad;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,39 @@ namespace WinFormsApp1
 
         private void MainWindowLoad(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello world");
+        }
+
+        private void MouseCoordinates(object sender, MouseEventArgs e)
+        {
+            if (Control.ModifierKeys == Keys.Control && e.Button == MouseButtons.Left)
+            {
+                this.Close();
+            }
+
+            int x = e.X;
+            int y = e.Y;
+
+            button1.Text = "X = " + x + ", Y = " + y;
+        }
+
+        private void RightClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                int x = Size.Width;
+                int y = Size.Height;
+
+                button1.Text = "Довжина = " + y + "\nШирина = " + x;
+            }
+        }
+
+        private void MouseMove(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+
+            label1.Text = "X = " + x;
+            label2.Text = "Y = " + y;
         }
     }
 }
