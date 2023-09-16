9840218f8dd2bafdf4111865deb4a9eb77565f25
Shoro749 <romanbro749@gmail.com>
2023-09-16 15:53:13 +0300

    Task 1 completed

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,27 @@ namespace WinFormsApp1
 
         private void MainWindowLoad(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello world");
+            string text = "Ім'я: Андрій";
+            string title = "Resume";
+            int temp = text.Length;
+            MessageBox.Show(text, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            text = "Вік: 30 років";
+            temp += text.Length;
+            MessageBox.Show(text, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            text = "Освіта: Бакалавр, галузь не визначена";
+            temp += text.Length;
+            MessageBox.Show(text, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            text = "Досвід роботи: Півтора роки у сфері послуг";
+            temp += text.Length;
+            MessageBox.Show(text, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            text = "Захоплення: Спорт, читання, подорожі";
+            temp += text.Length;
+            MessageBox.Show(text, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            text = "Мета: Розвиватися і покращувати свої навички в різних сферах.";
+            temp += text.Length;
+            title = Convert.ToString(temp/6);
+            MessageBox.Show(text, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
         }
     }
 }
